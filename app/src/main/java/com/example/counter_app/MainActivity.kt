c7cf672bfe69120d6b51eecfe27dcfb9caca8271
johnaeris <barrotjohnaeris@gmail.com>
2022-11-12 23:19:04 +0800

    Add reset button

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.counter_app
 
 import android.os.Bundle
-import android.provider.Settings.Global
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -9,19 +9,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.counter_app.ui.theme.CounterAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,13 +48,36 @@ fun MainScreen() {
         mutableStateOf(0)
     }
 
+    val context = LocalContext.current
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Card(
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable {
+                    initialCount = 0
+                    Toast.makeText(context, "Reset", Toast.LENGTH_SHORT).show()
+                },
+            elevation = 8.dp
+        ) {
+            Image(
+                painter = painterResource(R.drawable.reset_ic),
+                contentDescription = "Reset button",
+                Modifier
+                    .background(colorResource(R.color.beau_blue))
+                    .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                    .size(50.dp)
+                    .align(Alignment.CenterHorizontally)
 
-
+            )
+        }
+        
+        Spacer(modifier = Modifier.height(200.dp))
 
         Text(
             text = initialCount.toString(),
@@ -75,7 +100,9 @@ fun MainScreen() {
                 Image(
                     painter = painterResource(R.drawable.minus_ic),
                     contentDescription = "Minus button",
-                    Modifier.size(70.dp).background(Color.White)
+                    Modifier
+                        .size(70.dp)
+                        .background(Color.White)
                 )
             }
             
@@ -93,7 +120,9 @@ fun MainScreen() {
                 Image(
                     painter = painterResource(R.drawable.add_ic),
                     contentDescription = "Add button",
-                    Modifier.size(70.dp).background(Color.White)
+                    Modifier
+                        .size(70.dp)
+                        .background(Color.White)
                 )
             }
         }
