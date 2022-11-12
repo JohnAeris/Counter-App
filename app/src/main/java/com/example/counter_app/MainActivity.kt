88c6371539f6c41e8d98905531cc3fcd321dae6c
johnaeris <barrotjohnaeris@gmail.com>
2022-11-12 22:52:31 +0800

    Add increment and decrement

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -1,28 +1,27 @@
 package com.example.counter_app
 
 import android.os.Bundle
+import android.provider.Settings.Global
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.counter_app.ui.theme.CounterAppTheme
-import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,27 +42,60 @@ fun App() {
 @Composable
 fun MainScreen() {
 
-    var initial_number = 0
+    var initialCount by remember {
+        mutableStateOf(0)
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+
+
         Text(
-            text = initial_number.toString(),
-            fontSize = 150.sp,
-            fontWeight = FontWeight.Bold,
+            text = initialCount.toString(),
+            fontSize = 100.sp,
+            fontWeight = FontWeight.SemiBold,
         )
         
-        Spacer(modifier = Modifier.height(100.dp))
+        Spacer(modifier = Modifier.height(50.dp))
 
         Row() {
-            MinusButton()
+            Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable {
+                        initialCount--
+                    },
+                elevation = 8.dp,
+                border = BorderStroke(5.dp, Color.Black)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.minus_ic),
+                    contentDescription = "Minus button",
+                    Modifier.size(70.dp).background(Color.White)
+                )
+            }
             
             Spacer(modifier = Modifier.width(130.dp))
-            
-            AddButton()
+
+            Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable {
+                        initialCount++
+                    },
+                elevation = 8.dp,
+                border = BorderStroke(5.dp, Color.Black)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.add_ic),
+                    contentDescription = "Add button",
+                    Modifier.size(70.dp).background(Color.White)
+                )
+            }
         }
     }
 }
@@ -72,7 +104,11 @@ fun MainScreen() {
 @Composable
 fun AddButton() {
     Card(
-        modifier = Modifier.wrapContentSize(),
+        modifier = Modifier
+            .wrapContentSize()
+            .clickable {
+
+            },
         elevation = 8.dp,
         border = BorderStroke(5.dp, Color.Black)
     ) {
